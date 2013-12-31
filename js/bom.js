2540a61398536eea2b382b6085ba5b9ddbdc2e6d
syungkim <syung2nara@naver.com>
2013-12-31 15:15:21 +0900

    DATE 관련 함수 추가 (날짜 차이 계산 )

--- js/bom.js
+++ js/bom.js
@@ -21,3 +21,20 @@ $.extend({
 	}
 });
 
+
+/*
+ * 예약일과 현재일의 차이 계산
+ * 날짜 차이 계산 함수
+ * date1 : (미래)기준 날짜(YYYY-MM-DD), date2 : (현재)대상 날짜(YYYY-MM-DD)
+ */
+function getDateDiff(date1,date2){
+	var arrDate1 = date1.split("-");
+	var getDate1 = new Date(parseInt(arrDate1[0]),parseInt(arrDate1[1])-1,parseInt(arrDate1[2]));
+	var arrDate2 = date2.split("-");
+	var getDate2 = new Date(parseInt(arrDate2[0]),parseInt(arrDate2[1])-1,parseInt(arrDate2[2]));
+
+	var getDiffTime = getDate1.getTime() - getDate2.getTime();
+
+	return Math.floor(getDiffTime / (1000 * 60 * 60 * 24));
+}
+
