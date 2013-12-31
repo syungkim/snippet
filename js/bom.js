f5a84efc70ee797bca5d5db48474f3c6d3102cb8
syungkim <syung2nara@naver.com>
2013-12-31 16:21:00 +0900

    DATE관련 함수 추가 _ 김시형

--- js/bom.js
+++ js/bom.js
@@ -28,6 +28,12 @@ $.extend({
  * date1 : (미래)기준 날짜(YYYY-MM-DD), date2 : (현재)대상 날짜(YYYY-MM-DD)
  * @example :  getDateDiff('2014-01-01','2013-12-31');
  *****************************/
+/**
+ * 예약일과 현재일의 차이 계산
+ * @param date1
+ * @param date2
+ * @returns {number}
+ */
 function getDateDiff(date1,date2){
 	var arrDate1 = date1.split("-");
 	var getDate1 = new Date(parseInt(arrDate1[0]),parseInt(arrDate1[1])-1,parseInt(arrDate1[2]));
@@ -39,3 +45,20 @@ function getDateDiff(date1,date2){
 	return Math.floor(getDiffTime / (1000 * 60 * 60 * 24));
 }
 
+/***
+ * 날짜 계산 함수
+ * @param targetDate : 기준 날짜(Date 형)
+ * @param dayPrefix : : 계산할 일수(정수형)
+ * @returns {Date}
+ * @example
+ * 	var dateObj = new Date();
+ 	var diffDate = getAddDay(dateObj,-2);
+ */
+
+function getAddDay(targetDate, dayPrefix)
+{
+	var newDate = new Date( );
+	var processTime = targetDate.getTime()+(parseInt(dayPrefix)*24*60*60*1000);
+	newDate.setTime(processTime);
+	return newDate;
+}
